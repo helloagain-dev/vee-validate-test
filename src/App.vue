638d474f527c09475b7d8b97ded5aefbd34e752e
Michael Schoendorfer <michael.schoendorfer@helloagain.at>
2017-08-11 16:28:07 +0200

    Add vee-validate

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,34 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <custom-input v-validate="'required|email'" data-vv-value-path="innerValue" data-vv-name="custom" label="Email"
+                      :has-error="errors.has('custom')">
+        </custom-input>
+        <span v-show="errors.has('custom')" class="help is-danger">{{ errors.first('custom') }}</span>
+        <button @click="validate" type="button" class="button is-primary">Validate All</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import CustomInput from '../src/components/CustomInput.vue'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    CustomInput
+  },
+  methods: {
+    validate() {
+      this.$validator.validateAll().then(result => {
+          // eslint-disable-next-line
+          alert(`Validation Result: ${result}`);
+      });
+    }
+  }
 }
 </script>
 
